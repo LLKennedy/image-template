29393e6a2ccfdef906a5aeeb0fe02ca1e6794eb3
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-07-01 21:29:27 +1000

    Create a basic LitElement component

--- src/imagetemplate.js
+++ src/imagetemplate.js
@@ -1,21 +1,29 @@
-(function(window) {
-    'use strict'
+import { LitElement, html, customElement, property } from 'lit-element';
 
-    /**
-     * Core
-     */
-    const imageTemplate = function() {
-        const canvas = {
-            /**
-             * Init canvas
-             */
-            init() {
+@customElement('image-template')
+class ImageTemplate extends LitElement {
+	/**
+	 * Width is the width of the canvas
+	 */
+	@property()
+	width = 'width';
+	
+	/**
+	 * Height is the height of the canvas
+	 */
+	@property()
+	height = 'height';
 
-            }
-        }
-    }
+	/**
+	 * Render defines the HTML template
+	 */
+	render(){
+		console.log(this.width, this.height)
+		return html`
+		<p> width=${this.width} height=${this.height}</p>
+		`;
+	}
+}
 
-    if(typeof(window.imageTemplate) === 'undefined') {
-        window.imageTemplate = imageTemplate();
-    }
-})(window);
+// Register with the browser
+customElements.define('image-template', ImageTemplate);
